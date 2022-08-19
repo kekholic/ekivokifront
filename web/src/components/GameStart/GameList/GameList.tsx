f736e66b7a70e4d01b5eb77fa8c834b57845ce27
IvanM <mahankovage@gmail.com>
2022-08-19 10:40:24 +0300

    empty rooms

--- web/src/components/GameStart/GameList/GameList.tsx
+++ web/src/components/GameStart/GameList/GameList.tsx
@@ -3,27 +3,30 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 import { Link } from 'react-router-dom';
-import { useAppDispatch, useAppSelector } from '../../../hooks/redux';
+import {
+  // useAppDispatch,
+  useAppSelector
+} from '../../../hooks/redux';
 import { connectedToTheGame } from '../../../lib/game/gameUpdate';
 import { IGame } from '../../../models/IGame';
 import socket from '../../../socket';
 import ACTIONS from '../../../actions/wsActions';
-import {
-  getGame,
-} from '../../../store/reducers/actionCreators';
+// import {
+//   getGame,
+// } from '../../../store/reducers/actionCreators';
 import style from './GameList.module.css';
-import GAME_STATUS from '../../../actions/gameStatus';
+// import GAME_STATUS from '../../../actions/gameStatus';
 
 export default function GameList() {
-  const { games } = useAppSelector((store) => store.allGame);
+  // const { games } = useAppSelector((store) => store.allGame);
   const user = useAppSelector((store) => store.user);
-  const dispatch = useAppDispatch();
+  // const dispatch = useAppDispatch();
   const navigate = useNavigate();
   // TODO:
   // запрос с базы данных активных игр!
-  useEffect(() => {
-    dispatch(getGame());
-  }, []);
+  // useEffect(() => { //Здесь был список игр с пустыми комнатами
+  //   dispatch(getGame());
+  // }, []);
 
   const [activeLobby, setActiveLobby] = useState([]);
 
@@ -45,30 +48,30 @@ export default function GameList() {
   return (
     <div className={style.listContent}>
       {
-      activeLobby
-        ? activeLobby.map((gameInner: IGame) => (
-          <div className={style.listItem} key={gameInner.id}>
-            <span className={style.listTitle}>{gameInner.title}</span>
-            <span className={style.listCount}>
-              {gameInner.countPlayers}
-              /
-              {gameInner.maxPlayers}
+        activeLobby
+          ? activeLobby.map((gameInner: IGame) => (
+            <div className={style.listItem} key={gameInner.id}>
+              <span className={style.listTitle}>{gameInner.title}</span>
+              <span className={style.listCount}>
+                {gameInner.countPlayers}
+                /
+                {gameInner.maxPlayers}
+              </span>
+              <input className={style.listInput} type="text" name="password" placeholder="Введите пароль" />
+              <button className={style.listSubmit} type="submit" onClick={() => handleClick(gameInner)}>Выбрать игру</button>
+            </div>
+          ))
+          : (
+            <span className={style.listError}>
+              Нет открытых лобби,
+              {' '}
+              <Link className={style.listErrorLink} to="./../new">создайте</Link>
+              {' '}
+              лобби.
             </span>
-            <input className={style.listInput} type="text" name="password" placeholder="Введите пароль" />
-            <button className={style.listSubmit} type="submit" onClick={() => handleClick(gameInner)}>Выбрать игру</button>
-          </div>
-        ))
-        : (
-          <span className={style.listError}>
-            Нет открытых лобби,
-            {' '}
-            <Link className={style.listErrorLink} to="./../new">создайте</Link>
-            {' '}
-            лобби.
-          </span>
-        )
-    }
-      {
+          )
+      }
+      {/* { //Здесь был список игр с пустыми комнатами
       games.length
         ? games.filter((game) => game.status === GAME_STATUS.CREATED).map((gameInner: IGame) => (
           <div className={style.listItem} key={gameInner.id}>
@@ -92,7 +95,7 @@ export default function GameList() {
             лобби.
           </span>
         )
-    }
+    } */}
     </div>
   );
 }
